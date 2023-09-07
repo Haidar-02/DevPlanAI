387f234a94bca13e9833d53cd40a4c60b74eb5c0
HaidarMestrah <10120319@mu.edu.lb>
2023-09-08 02:02:39 +0300

    SERVER: MIGRATION

--- server/database/migrations/2014_10_12_000000_create_users_table.php
+++ server/database/migrations/2014_10_12_000000_create_users_table.php
@@ -9,24 +9,107 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
+    public function up()
     {
+        // User Types table
+        Schema::create('user_types', function (Blueprint $table) {
+            $table->id();
+            $table->string('user_type');
+            $table->timestamps();
+        });
+        // Users table
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->unsignedBigInteger('user_type_id');
+            $table->foreign('user_type_id')->references('id')->on('user_types');
+            $table->longText('profile_picture')->nullable();
+            $table->timestamps();
+        });
+
+        
+
+        // Projects table
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->date('deadline');
+            $table->boolean('is_done')->default(false);
+            $table->date('finish_date')->nullable();
+            $table->unsignedBigInteger('project_manager_id');
+            $table->foreign('project_manager_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+
+        // Tasks table
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->date('deadline');
+            $table->boolean('is_done')->default(false);
+            $table->unsignedBigInteger('assignee_id')->nullable();
+            $table->foreign('assignee_id')->references('id')->on('users');
+            $table->unsignedBigInteger('project_id');
+            $table->foreign('project_id')->references('id')->on('projects');
+            $table->timestamps();
+        });
+
+        // Comments table
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->text('content');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->unsignedBigInteger('task_id');
+            $table->foreign('task_id')->references('id')->on('tasks');
+            $table->timestamps();
+        });
+
+        // Teams table
+        Schema::create('teams', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->unsignedBigInteger('project_id');
+            $table->foreign('project_id')->references('id')->on('projects');
+            $table->timestamps();
+        });
+
+        // Contribution Requests table
+        Schema::create('contribution_requests', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->unsignedBigInteger('project_id');
+            $table->foreign('project_id')->references('id')->on('projects');
+            $table->timestamps();
+        });
+
+        // Notifications table
+        Schema::create('notifications', function (Blueprint $table) {
+            $table->id();
+            $table->text('message');
+            $table->boolean('is_read')->default(false);
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
+        Schema::dropIfExists('notifications');
+        Schema::dropIfExists('contribution_requests');
+        Schema::dropIfExists('teams');
+        Schema::dropIfExists('comments');
+        Schema::dropIfExists('tasks');
+        Schema::dropIfExists('projects');
+        Schema::dropIfExists('user_types');
         Schema::dropIfExists('users');
     }
 };
