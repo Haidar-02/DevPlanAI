4c6a04ab508cf951d7ac4667061382288b5144aa
HaidarMestrah <10120319@mu.edu.lb>
2023-09-19 04:47:46 +0300

    server: admin controller functionalities

---
+++ server/app/Http/Controllers/AdminController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Project;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    public function getAllProjects()
+    {
+        $projects = Project::all();
+
+        return response()->json([
+            'status' => 'success',
+            'projects' => $projects,
+        ]);
+    }
+
+    public function searchAllProject(Request $request)
+    {
+        $search = $request->input('search');
+        $projects = Project::where('title', 'like', "%$search%")
+            ->orWhere('description', 'like', "%$search%")
+            ->get();
+
+        return response()->json([
+            'status' => 'success',
+            'projects' => $projects,
+        ]);
+    }
+
+    public function deleteProject($projectId)
+    {
+        try {
+            $project = Project::findOrFail($projectId);
+            $project->delete();
+
+            return response()->json([
+                'status' => 'success',
+                'message' => 'Project deleted successfully.',
+            ]);
+        } catch (\Exception $e) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'An error occurred while deleting the project.',
+            ]);
+        }
+    }
+
+    public function getAllUsers()
+    {
+        $users = User::all();
+
+        return response()->json([
+            'status' => 'success',
+            'users' => $users,
+        ]);
+    }
+
+    public function deleteUser($userId)
+    {
+        try {
+            $user = User::findOrFail($userId);
+            $user->delete();
+
+            return response()->json([
+                'status' => 'success',
+                'message' => 'User deleted successfully.',
+            ]);
+        } catch (\Exception $e) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'An error occurred while deleting the user.',
+            ]);
+        }
+    }
+}
