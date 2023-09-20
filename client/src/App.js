96cd67e1a3258cfb1d1b0ee5539c8682d82672d7
HaidarMestrah <10120319@mu.edu.lb>
2023-09-20 19:32:29 +0300

    client : routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,19 @@
+import Dashboard from "./Pages/Dashboard/Dashboard";
+import LandingPage from "./Pages/LandingPage/LandingPage";
+import LoginSignup from "./Pages/LoginSignUp/LoginSignup";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <div>
+        <Switch>
+          <Route path="/" exact component={LandingPage} />
+          <Route path="/auth" component={LoginSignup} />
+          <Route path="/Dashboard" component={Dashboard} />
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
