093c3ac84602fa85af9e50a1d7e0476140ac6f9c
HaidarMestrah <10120319@mu.edu.lb>
2023-09-21 02:50:09 +0300

    Client :Login Form Validation

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/Dashboard" element={<Dashboard />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
   );
--- client/src/Components/LoginForm/LoginForm.jsx
+++ client/src/Components/LoginForm/LoginForm.jsx
@@ -1,31 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const LoginForm = () => {
   const navigate = useNavigate();
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleShowPasswordToggle = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const handleLogin = () => {
+    // Basic validation (you can add more complex validation as needed)
+    if (formData.email.trim() === "" || formData.password.trim() === "") {
+      alert("Please enter both email and password.");
+    } else {
+      // Perform the login logic here (e.g., sending a request to the server)
+      // After successful login, you can navigate to another page
+      // For now, let's navigate to a dummy page "/dashboard"
+      navigate("/dashboard");
+    }
+  };
+
   return (
     <div className="flex flex-col items-center justify-center m-5 w-full">
       <input
         type="email"
         name="email"
-        placeholder="email"
+        placeholder="Email"
         autoComplete="off"
+        value={formData.email}
+        onChange={handleInputChange}
         className="m-3 placeholder:text-[#4F5D75] bg-transparent px-3 py-1 text-base w-full border-b-2 border-b-gray-800 outline-none transition-all focus:border-b-[#4F5D75]"
       />
       <input
-        type="password"
-        name="pasword"
-        placeholder="password"
+        type={showPassword ? "text" : "password"}
+        name="password"
+        placeholder="Password"
+        value={formData.password}
+        onChange={handleInputChange}
         className="m-3 placeholder:text-[#4F5D75] bg-transparent px-3 py-1 text-base w-full border-b-2 border-b-gray-800 outline-none transition-all focus:border-b-[#4F5D75]"
       />
       <div className="flex self-start mt-3 cursor-pointer">
-        <input type="checkBox" name="showPass" id="showPass" />
+        <input
+          type="checkbox"
+          name="showPass"
+          id="showPass"
+          onChange={handleShowPasswordToggle}
+        />
         <label htmlFor="showPass" className="text-sm text-gray-500 ml-1">
           Show Password
         </label>
       </div>
 
-      <button className="bg-[#4F5D75] px-3 py-1 w-5/6 text-white mt-8 rounded-full text-base hover:bg-black transition-all">
+      <button
+        onClick={handleLogin}
+        className="bg-[#4F5D75] px-3 py-1 w-5/6 text-white mt-8 rounded-full text-base hover:bg-black transition-all"
+      >
         Login
       </button>
 
