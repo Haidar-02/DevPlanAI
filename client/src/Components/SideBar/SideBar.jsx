3b966441606ffd13f0e68311bef23c02fd82dd58
HaidarMestrah <10120319@mu.edu.lb>
2023-09-24 23:04:20 +0300

    Client: Modifying navbar

--- client/src/Components/SideBar/SideBar.jsx
+++ client/src/Components/SideBar/SideBar.jsx
@@ -2,33 +2,68 @@ import React from "react";
 import "./SideBar.css";
 import logo from "../../Assets/LogoFull.png";
 import DashboardIcon from "@mui/icons-material/Dashboard";
+import AccountTreeIcon from "@mui/icons-material/AccountTree";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import LogoutIcon from "@mui/icons-material/Logout";
+import CreateBoxIcon from "@mui/icons-material/AddBox";
+import GroupAddIcon from "@mui/icons-material/GroupAdd";
+import AdminPanelSettingsIcon from "@mui/icons-material/AdminPanelSettings";
 import SidebarItem from "./SidebarItem";
+import { Avatar } from "@mui/material";
 
 const SideBar = () => {
   return (
     <div className=" w-60 h-screen bg-[#2D3142] relative">
-      <div className="w-full px-3 py-5 flex flex-col items-center justify-between">
+      <div className="w-full px-3 py-5 flex flex-col items-center justify-between h-full">
         <div className="flex items-center justify-center">
           <img src={logo} alt="" className="w-4/5" />
         </div>
         <div className="navItems w-full">
-          <ul className="flex flex-col items-center justify-center mt-10 w-full">
+          <ul className="flex flex-col items-center justify-center w-full">
             <SidebarItem
               text={"Dashboard"}
               icon={DashboardIcon}
               link={"/dashboard"}
             />
             <SidebarItem
-              text={"Dashboard"}
-              icon={DashboardIcon}
-              link={"/dashboard"}
+              text={"My Projects"}
+              icon={AccountTreeIcon}
+              link={"/projects"}
             />
             <SidebarItem
-              text={"Dashboard"}
-              icon={DashboardIcon}
-              link={"/dashboard"}
+              text={"Create"}
+              icon={CreateBoxIcon}
+              link={"/create"}
+            />
+            <SidebarItem
+              text={"Team Requests"}
+              icon={GroupAddIcon}
+              link={"/contributions"}
+            />
+            <SidebarItem
+              text={"Notifications"}
+              icon={NotificationsIcon}
+              link={"/notifications"}
+            />
+            <SidebarItem
+              text={"Administration"}
+              icon={AdminPanelSettingsIcon}
+              link={"/admin"}
             />
           </ul>
+        </div>
+        <div className="w-full flex flex-col items-center justify-center">
+          <div className="w-full flex items-center gap-2 px-2 py-1 rounded-lg cursor-pointer text-white hover:bg-gray-200 hover:text-[#2D3142] transition-all">
+            <Avatar />
+            <div>
+              <h2 className="text-sm">FirstName LastName</h2>
+              <h2 className="text-xs opacity-80">Email</h2>
+            </div>
+          </div>
+          <div className="text-white flex items-center justify-start px-4 w-full hover:bg-white hover:text-red-500 py-2 cursor-pointer rounded-full transition-all my-2 relative">
+            <LogoutIcon />
+            <h2 className=" font-regular ml-2 tracking-wide">Logout</h2>
+          </div>
         </div>
       </div>
     </div>
--- client/src/Components/SideBar/SidebarItem.jsx
+++ client/src/Components/SideBar/SidebarItem.jsx
@@ -1,15 +1,15 @@
 import React, { createElement } from "react";
 import { useNavigate } from "react-router-dom";
 
-const SidebarItem = ({ text, link, icon, notify }) => {
+const SidebarItem = ({ text, link, icon, notify, isActive }) => {
   const navigate = useNavigate();
   return (
     <li
       onClick={() => navigate({ link })}
-      className="text-white flex items-center justify-start px-4 w-full hover:bg-white hover:text-[#2D3142] py-2 cursor-pointer rounded-full transition-all my-1 relative"
+      className="text-white flex items-center justify-start px-4 w-full hover:bg-white hover:text-[#2D3142] py-2 cursor-pointer rounded-full transition-all my-2 relative"
     >
       {createElement(icon)}
-      <h2 className=" font-medium ml-2 tracking-wide">{text}</h2>
+      <h2 className=" font-regular ml-2 tracking-wide">{text}</h2>
       <div className="absolute bg-red-500 w-2 h-2 rounded-full right-4"></div>
     </li>
   );
