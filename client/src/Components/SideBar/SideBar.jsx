b4301235b75dabef6b38caf70395be5fcbaff763
HaidarMestrah <10120319@mu.edu.lb>
2023-09-25 01:54:52 +0300

    Client:Linked navbar to backend

--- client/src/Components/SideBar/SideBar.jsx
+++ client/src/Components/SideBar/SideBar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./SideBar.css";
 import logo from "../../Assets/LogoFull.png";
 import DashboardIcon from "@mui/icons-material/Dashboard";
@@ -11,11 +11,46 @@ import AdminPanelSettingsIcon from "@mui/icons-material/AdminPanelSettings";
 import SidebarItem from "./SidebarItem";
 import { Avatar } from "@mui/material";
 import { useNavigate } from "react-router-dom";
+import { getUserGeneralInfo } from "../../Helpers/user.helper";
+import ErrorMessageComponent from "../EventComponents/ErrorComponent";
+import Lottie from "lottie-react";
+import loadingLottie from "../../Assets/LottieAssets/loading.json";
 
 const SideBar = () => {
   const navigate = useNavigate();
+  const clearMessage = () => {
+    setErrorMessage("");
+  };
+  const [errorMessage, setErrorMessage] = useState("");
+  const [userInfo, setUserInfo] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchUserInfo() {
+      try {
+        const response = await getUserGeneralInfo();
+        setUserInfo(response.data);
+        setIsLoading(false);
+        console.log(response);
+        if (response.data.status === "failed") {
+          setErrorMessage(response.data.message);
+        }
+      } catch (error) {
+        console.error("Error fetching user information: ", error);
+        setIsLoading(false);
+      }
+    }
+
+    fetchUserInfo();
+  }, []);
+
   return (
-    <div className=" w-60 h-screen bg-[#2D3142] relative">
+    <div className=" w-60 h-screen bg-[#2D3142]">
+      {isLoading && (
+        <div className="fixed top-0 left-0 w-full h-1">
+          <div className="h-full bg-white animate-loading-bar shadow-lg"></div>
+        </div>
+      )}
       <div className="w-full px-3 py-5 flex flex-col items-center justify-between h-full">
         <div className="flex items-center justify-center">
           <img src={logo} alt="" className="w-4/5" />
@@ -55,25 +90,43 @@ const SideBar = () => {
           </ul>
         </div>
         <div className="w-full flex flex-col items-center justify-center">
-          <div
-            onClick={() => navigate("/profile")}
-            className="w-full flex items-center gap-2 px-2 py-1 rounded-lg cursor-pointer text-white hover:bg-gray-200 hover:text-[#2D3142] transition-all"
-          >
-            <Avatar />
-            <div className=" max-w-[170px]">
-              <h2 className="text-sm overflow-hidden overflow-ellipsis whitespace-nowrap">
-                FirstName LastName
-              </h2>
-              <h2 className="text-xs opacity-80 overflow-hidden overflow-ellipsis whitespace-nowrap">
-                Email@example.com
-              </h2>
+          {userInfo && userInfo.user ? (
+            <div
+              onClick={() => navigate("/profile")}
+              className="w-full flex items-center gap-2 px-2 py-1 rounded-lg cursor-pointer text-white hover:bg-gray-200 hover:text-[#2D3142] transition-all"
+            >
+              {userInfo.user.profile_picture ? (
+                <Avatar src={userInfo.user.profile_picture} />
+              ) : (
+                <Avatar {...stringAvatar("FirstName LastName")} />
+              )}
+              <div className="max-w-[160px]">
+                <h2 className="text-sm overflow-hidden overflow-ellipsis whitespace-nowrap">
+                  {userInfo.user.first_name} {userInfo.user.last_name}
+                </h2>
+                <h2 className="text-xs opacity-80 overflow-hidden overflow-ellipsis whitespace-nowrap">
+                  {userInfo.user.email}
+                </h2>
+              </div>
             </div>
-          </div>
+          ) : (
+            <div>
+              <Lottie animationData={loadingLottie} />
+            </div>
+          )}
           <div className="text-white flex items-center justify-start px-4 w-full hover:bg-white hover:text-red-500 py-2 cursor-pointer rounded-lg transition-all my-2 relative">
             <LogoutIcon />
             <h2 className="font-regular ml-2 tracking-wide">Logout</h2>
           </div>
         </div>
+      </div>
+      <div className="absolute top-10 right-10 z-50">
+        {errorMessage && (
+          <ErrorMessageComponent
+            message={errorMessage}
+            clearMessage={clearMessage}
+          />
+        )}
       </div>
     </div>
   );
--- client/src/Helpers/user.helper.js
+++ client/src/Helpers/user.helper.js
@@ -1,2 +1,17 @@
 import axios from "axios";
+import { auth } from "./auth.helpers";
 const baseUrl = "http://127.0.0.1:8000/api/";
+
+async function getUserGeneralInfo() {
+  try {
+    const res = await axios.get(`${baseUrl}user/getUserGeneralInfo`, auth());
+    if (res.status === 200) {
+      const data = res.data;
+      return { data };
+    }
+  } catch (error) {
+    throw error;
+  }
+}
+
+export { getUserGeneralInfo };
