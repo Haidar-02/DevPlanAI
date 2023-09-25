ddbe42aaa945b077206b7934c75347126580ae7e
HaidarMestrah <10120319@mu.edu.lb>
2023-09-26 01:37:44 +0300

    Client: modified notification compnts

--- client/src/Components/NotificationComponents/ReadComponent.jsx
+++ client/src/Components/NotificationComponents/ReadComponent.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const ReadComponent = () => {
-  return <div>Read Notifications</div>;
+  return (
+    <div className="w-full p-5 flex flex-col gap-2 rounded-lg bg-white">
+      <h2 className=" font-bold">Read Notifications</h2>
+      <div className="flex items-center justify-between p-3 bg-gray-200 hover:opacity-80 transition-all">
+        <p className="text-sm">message</p>
+      </div>
+    </div>
+  );
 };
 
 export default ReadComponent;
--- client/src/Components/NotificationComponents/UnreadComponent.jsx
+++ client/src/Components/NotificationComponents/UnreadComponent.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const UnreadComponent = () => {
-  return <div>Unread notifications</div>;
+  return (
+    <div className="w-full p-5 flex flex-col gap-2 rounded-lg bg-white">
+      <h2 className=" font-bold">Unread Notifications</h2>
+      <div className="flex items-center justify-between p-3 bg-gray-200 hover:opacity-80 transition-all">
+        <p className="text-sm">message</p>
+        <button className="text-sm p-2 bg-green-600 text-white rounded-md hover:bg-white hover:text-green-600 transition-all">
+          Mark as read
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default UnreadComponent;
